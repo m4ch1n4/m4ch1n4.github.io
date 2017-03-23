36436f30131753cccf65e1b1b6dcb903ff499f61
r.j.m.meneses@gmail.com <toor@MacBook-Pro-de-Ruben.local>
2017-03-22 21:16:21 -0600

    the next

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-
+var anim;
+var animData = {
+  container: document.getElementById('bodymovin'),
+  renderer: 'pixi',
+  loop: true,
+  autoplay: true,
+  autoloadSegments: true,
+  rendererSettings: {
+    progressiveLoad: false
+  },
+  path: 'h'
+};
+anim = bodymovin.loadAnimation(animData);
