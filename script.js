774bff827b1dd2655fc362e7af6af82722a4c80a
m4ch1n4 <r.j.m.meneses@gmail.com>
2019-12-10 22:38:27 -0600

    a
    run

--- script.js
+++ script.js
@@ -1,45 +1,41 @@
-window.onload = play();
-document.getElementById('#logo').style.color = '#00FF00';
-function play() {
-	var l = Snap('#logo');
-	var p = l.select('path');
-    l.clear();
-	l.append(p);
-	p.attr();
+$(document).ready(function(){
+var theLetters = "1234567890#%&^+=-"; //You can customize what letters it will cycle through
+var ctnt = "M4CH1N4"; // Your text goes here
+var speed = 150; // ms per frame
+var increment = 8; // frames per step. Must be >2
 
-	setTimeout( function() {
-		// modify this one line below, and see the result !
-		var logoTitle = 'M4CH1N4';
-		var logoRandom = '';
-		var logoTitleContainer = l.text(0, '98%', '');
-		
-		var possible = "-+*/|}{[]~\\\":;?/.><=+-_)(*&^%$#@!)}";
-		
-		
-		logoTitleContainer.attr({
-			fontSize: 180,
-			fontFamily: 'arial',
-			fontWeight: '500',
-			fill:"white",
-            
-			
-		});
-
-		function generateRandomTitle(i, logoRandom) {
-			setTimeout( function() {
-				logoTitleContainer.attr({ text: logoRandom });
-			}, i*70 );
-		}
-
-		for( var i=0; i < logoTitle.length+1; i++ ) {
-			logoRandom = logoTitle.substr(0, i);
-			for( var j=i; j < logoTitle.length; j++ ) { 
-				logoRandom += possible.charAt(Math.floor(Math.random() * possible.length)); 
-			}
-			generateRandomTitle(i, logoRandom);
-			logoRandom = '';
-		}
-
-	}, 450 );
-
+    
+var clen = ctnt.length;       
+var si = 0;
+var stri = 0;
+var block = "";
+var fixed = "";
+//Call self x times, whole function wrapped in setTimeout
+(function rustle (i) {          
+setTimeout(function () {
+  if (--i){rustle(i);}
+  nextFrame(i);
+  si = si + 1;        
+}, speed);
+})(clen*increment+1); 
+function nextFrame(pos){
+  for (var i=0; i<clen-stri; i++) {
+    //Random number
+    var num = Math.floor(theLetters.length * Math.random());
+    //Get random letter
+    var letter = theLetters.charAt(num);
+    block = block + letter;
+  }
+  if (si == (increment-1)){
+    stri++;
+  }
+  if (si == increment){
+  // Add a letter; 
+  // every speed*10 ms
+  fixed = fixed +  ctnt.charAt(stri - 1);
+  si = 0;
+  }
+  $("#output").html(fixed + block);
+  block = "";
 }
+});
