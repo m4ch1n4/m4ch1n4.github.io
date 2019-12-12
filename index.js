ccbb0bbab65a942fecf3730482041715b3d5783d
m4ch1n4 <r.j.m.meneses@gmail.com>
2019-12-11 23:59:55 -0600

    6

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// JavaScript Document
+
+var animData = {
+        container: document.getElementById('tainer'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+	    path:'data.js'
+    };
+  
