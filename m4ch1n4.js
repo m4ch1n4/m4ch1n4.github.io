bd6cc6826aeef8bfbaa8443127efd34d81ff7306
m4ch1n4 <r.j.m.meneses@gmail.com>
2019-12-12 03:50:55 -0600

    g

--- m4ch1n4.js
+++ m4ch1n4.js
@@ -29,7 +29,7 @@
 //  loading 1 m4ch1n4
 
   var myEar;
-  function mrRop() {
+  function mrRop () {
   myEar = setTimeout( showPage, 5000);
   }
 	
@@ -38,48 +38,5 @@
   document.getElementById("mySiv").style.display = "block";
 }
 
-
- // m4ch1n4 intro logo
-$(document).ready(function(){
-var theLetters = "1234567890#%&^+=-"; //You can customize what letters it will cycle through
-var ctnt = "M4CH1N4"; // Your text goes here
-var speed = 150; // ms per frame
-var increment = 8; // frames per step. Must be >2
-
-    
-var clen = ctnt.length;       
-var si = 0;
-var stri = 0;
-var block = "";
-var fixed = "";
-//Call self x times, whole function wrapped in setTimeout
-(function rustle (i) {          
-setTimeout(function () {
-  if (--i){rustle(i);}
-  nextFrame(i);
-  si = si + 1;        
-}, speed);
-})(clen*increment+1); 
-function nextFrame(pos){
-  for (var i=0; i<clen-stri; i++) {
-    //Random number
-    var num = Math.floor(theLetters.length * Math.random());
-    //Get random letter
-    var letter = theLetters.charAt(num);
-    block = block + letter;
-  }
-  if (si == (increment-1)){
-    stri++;
-  }
-  if (si == increment){
-  // Add a letter; 
-  // every speed*10 ms
-  fixed = fixed +  ctnt.charAt(stri - 1);
-  si = 0;
-  }
-  $("#output").html(fixed + block);
-  block = "";
-}
-});
 
 // bodymoving
